e9eb17901f0fe8deef35eb69d46f099fa02fd83f
aaronsatko <agsatko@gmail.com>
2023-12-16 22:07:24 -0500

    mse

--- loss/meansquareerror.go
+++ loss/meansquareerror.go
@@ -1,1 +1,19 @@
 package loss
+
+import "math"
+
+func MeanSquareError(yTrue, yPred []float64) float64 {
+	if len(yTrue) != len(yPred) {
+		panic("Input arrays must have the same length")
+	}
+
+	sumSqError := 0.0
+
+	for i := 0; i < len(yTrue); i++ {
+		sqError := math.Pow(yTrue[i]-yPred[i], 2)
+		sumSqError += sqError
+	}
+
+	mse := sumSqError / float64(len(yTrue))
+	return mse
+}
