2690433c129f0a17c92e21eb001a7e9b9aae46f5
aaronsatko <agsatko@gmail.com>
2023-12-17 02:37:48 -0500

    conv layer done

--- layer/convolutional.go
+++ layer/convolutional.go
@@ -1,1 +1,17 @@
 package layer
+
+type ConvLayer struct {
+	*Layer
+	KernelSize int
+	Stride     int
+}
+
+func NewConvLayer(inputSize, numNeurons, kernelSize, stride int, activation ActivationFunction) *ConvLayer {
+	layer := NewLayer(inputSize, numNeurons, activation)
+
+	return &ConvLayer{
+		Layer:      layer,
+		KernelSize: kernelSize,
+		Stride:     stride,
+	}
+}
