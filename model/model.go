4a8cae84e10a09b7f17ed1292a0c6101d0d67b36
aaronsatko <agsatko@gmail.com>
2023-12-17 22:29:05 -0500

    cleanup

--- model/model.go
+++ model/model.go
@@ -1,79 +1,110 @@
 package model
 
 import (
-	"errors"
-
-	"github.com/aaronsatko/gonenet/layer"
-	"github.com/aaronsatko/gonenet/optimizer"
+	"math"
 )
 
-type ModelState int
-
-const (
-	Uncompiled ModelState = iota
-	Compiled
-	Trained
-)
-
-type Model struct {
-	Layers    []layer.Layer
-	Loss      func([]float64, []float64) float64
-	Optimizer optimizer.Optimizer
-	State     ModelState
-}
-
-func NewModel() *Model {
-	return &Model{State: Uncompiled}
-}
-
-func (m *Model) AddLayer(l layer.Layer) error {
-	if len(m.Layers) > 0 {
-		lastLayer := m.Layers[len(m.Layers)-1]
-		// Assuming each layer struct has an OutputSize method
-		if l.InputSize() != lastLayer.OutputSize() {
-			return errors.New("input size of the new layer does not match the output size of the last layer")
-		}
+// Activation Functions
+func ReLU(x float64) float64 {
+	if x < 0 {
+		return 0
 	}
-	m.Layers = append(m.Layers, l)
-	return nil
+	return x
 }
 
-func (m *Model) Compile(lossFunc func([]float64, []float64) float64, opt optimizer.Optimizer) {
-	m.Loss = lossFunc
-	m.Optimizer = opt
-	m.State = Compiled
+func Sigmoid(X float64) float64 {
+	return 1 / (1 + math.Exp(-X))
 }
 
-func (m *Model) Fit(xTrain [][]float64, yTrain [][]float64, epochs int) error {
-	if m.State != Compiled {
-		return errors.New("model is not compiled")
+func ArrRelu(input []float64) []float64 {
+	output := make([]float64, len(input))
+	for i, val := range input {
+		output[i] = ReLU(val)
 	}
-	// Training logic here...
-	// Remember to update model state to Trained after successful training
-	m.State = Trained
-	return nil
+	return output
 }
 
-func (m *Model) Predict(x []float64) ([]float64, error) {
-	if m.State != Trained {
-		return nil, errors.New("model is not trained")
-	}
-	// Prediction logic here...
-	return nil, nil
+// Layer Types
+type ActivationFunction func(float64) float64
+
+type Layer struct {
+	InputSize  int
+	NumNeurons int
+	Weights    [][]float64
+	Biases     []float64
+	Activation ActivationFunction
 }
 
-// Placeholder for future method implementations
-func (m *Model) Evaluate(xTest [][]float64, yTest [][]float64) float64 {
-	// Evaluation logic here...
-	return 0.0
+type ConvLayer struct {
+	*Layer
+	KernelSize int
+	Stride     int
 }
 
-func (m *Model) Save(filePath string) error {
-	// Save model logic here...
-	return nil
+type DenseLayer struct {
+	*Layer
 }
 
-func (m *Model) Load(filePath string) error {
-	// Load model logic here...
-	return nil
+type RecurrentLayer struct {
+	*Layer
+	TimeSteps int
+}
+
+// Loss Functions
+func CrossEntropy(yTrue, yPred []float64) float64 {
+	// Implementation...
+}
+
+func MeanSquaredError(yTrue, yPred []float64) float64 {
+	// Implementation...
+}
+
+// Optimizers
+
+type AdamOptimizer struct {
+	// Fields...
+}
+
+type SGDOptimizer struct {
+	// Fields...
+}
+
+// Layer Initialization Functions
+func NewLayer(inputSize, numNeurons int, activation ActivationFunction) *Layer {
+	// Implementation...
+}
+
+func Conv(inputSize, numNeurons, kernelSize, stride int, activation ActivationFunction) *ConvLayer {
+	// Implementation...
+}
+
+func Dense(inputSize, numNeurons int, activation ActivationFunction) *DenseLayer {
+	// Implementation...
+}
+
+func Rec(inputSize, numNeurons, timeSteps int, activation ActivationFunction) *RecurrentLayer {
+	// Implementation...
+}
+
+// Optimizer Initialization Functions
+func Adam(learningRate, beta1, beta2, epsilon float64, weightShape [][]float64, biasShape []int) *AdamOptimizer {
+	// Implementation...
+}
+
+func SGD(learningRate float64) *SGDOptimizer {
+	// Implementation...
+}
+
+// Forward Pass Method for Layers
+func (l *Layer) Forward(input []float64) []float64 {
+	// Implementation...
+}
+
+// Update Methods for Optimizers
+func (adam *AdamOptimizer) Update(weights, gradients [][]float64, biases, biasGradients []float64) {
+	// Implementation...
+}
+
+func (sgd *SGDOptimizer) Update(weights, gradients [][]float64, biases, biasGradients []float64) {
+	// Implementation...
 }
