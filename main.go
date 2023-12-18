522b6bc9c175d550d2e9408dab8f08a483f4e6f6
aaronsatko <agsatko@gmail.com>
2023-12-17 22:56:08 -0500

    main

--- main.go
+++ main.go
@@ -6,29 +6,62 @@ import (
 	"github.com/aaronsatko/gonenet/activation"
 	"github.com/aaronsatko/gonenet/layer"
 	"github.com/aaronsatko/gonenet/loss"
+	"github.com/aaronsatko/gonenet/optimizer"
+	"github.com/aaronsatko/gonenet/datahandling"
 )
 
 func main() {
-    // Define the architecture of your neural network
-    inputLayer := layer.Dense(inputSize, numNeurons, activation.ReLU)
-    hiddenLayer := layer.Dense(numNeurons, hiddenLayerNeurons, activation.Sigmoid)
-    outputLayer := layer.Dense(hiddenLayerNeurons, outputNeurons, activation.Sigmoid)
+	// Load and preprocess data
+	data, labels := datahandling.LoadData("path/to/your/data.csv")
+	data = datahandling.Preprocess(data)
 
-    // Initialize an optimizer
-    adam := optimizer.Adam(learningRate, beta1, beta2, epsilon, weightShape, biasShape)
+	// Assume labels are in a format that needs encoding
+	encodedLabels, err := datahandling.EncodeLabels(labels)
+	if err != nil {
+		fmt.Println("Error encoding labels:", err)
+		return
+	}
 
-    // Training loop
-    for epoch := 0; epoch < numEpochs; epoch++ {
-        // Forward pass through each layer
-        inputLayerOutput := inputLayer.Forward(inputData)
-        hiddenLayerOutput := hiddenLayer.Forward(inputLayerOutput)
-        output := outputLayer.Forward(hiddenLayerOutput)
+	// Split data into training and test sets
+	trainData, testData, trainLabels, testLabels, err := datahandling.SplitData(data, encodedLabels, 0.8)
+	if err != nil {
+		fmt.Println("Error splitting data:", err)
+		return
+	}
 
-        // Calculate loss
-        lossValue := loss.CrossEntropy(trueOutput, output)
+	// Define nn architecture
+	inputLayer := layer.Dense(inputSize, numNeurons, activation.ReLU)
+	hiddenLayer := layer.Dense(numNeurons, hiddenLayerNeurons, activation.Sigmoid)
+	outputLayer := layer.Dense(hiddenLayerNeurons, outputNeurons, activation.Sigmoid)
 
-        // Backpropagation and optimization steps
-        // ...
-        adam.Update(...) // Update weights and biases
-    }
+	// Initialize optimizer
+	adam := optimizer.Adam(learningRate, beta1, beta2, epsilon, weightShape, biasShape)
+
+	// Training loop
+	for epoch := 0; epoch < numEpochs; epoch++ {
+		// Create batches
+		batches, _ := datahandling.Batch(trainData, batchSize)
+		labelBatches, _ := datahandling.Batch(trainLabels, batchSize)
+
+		for i, batch := range batches {
+			inputData := batch
+			trueOutput := labelBatches[i]
+
+			// Forward pass through each layer
+			inputLayerOutput := inputLayer.Forward(inputData)
+			hiddenLayerOutput := hiddenLayer.Forward(inputLayerOutput)
+			output := outputLayer.Forward(hiddenLayerOutput)
+
+			// Calculate loss
+			lossValue := loss.CrossEntropy(trueOutput, output)
+
+			// implement backprop
+
+			adam.Update(...) // Update weights and biases
+
+			// need to add functions for loss, accuracy
+		}
+	}
+
+	// need to eval model on test data
 }
