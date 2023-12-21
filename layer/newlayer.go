335f9ad4db6d2b5be6a23676a4fcba279069ddf4
aaronsatko <agsatko@gmail.com>
2023-12-20 23:46:46 -0500

    documentation

--- layer/newlayer.go
+++ layer/newlayer.go
@@ -4,9 +4,22 @@ import (
 	"math/rand"
 )
 
+// Defines the type for activation function.
 type ActivationFunction func(float64) float64
 
-// Layer is a base type that defines common properties of all layer types
+// Layer is a base type that defines common properties of all layer types in a neural network.
+//
+// Fields:
+//   - InputSize: Number of inputs the layer receives. It depends on the size of the previous layer or the feature space.
+//   - NumNeurons: Number of neurons (or units) in the layer.
+//   - Weights: A 2D slice of float64 representing the weights associated with each neuron and input.
+//   - Biases: A slice of float64 representing the bias for each neuron.
+//   - Activation: The activation function used by the neurons in the layer.
+//
+// Usage:
+//   - This base type is used to create various specific types of layers like dense, convolutional, recurrent, etc.
+//   - It encapsulates the common functionalities and properties needed for different layer types.
+
 type Layer struct {
 	InputSize  int
 	NumNeurons int
@@ -15,7 +28,21 @@ type Layer struct {
 	Activation ActivationFunction
 }
 
-// NewLayer is a generic function for creating layers
+// NewLayer is a generic function for creating a new layer with specified properties.
+// Initializes a layer with random weights and biases.
+//
+// Arguments:
+//   - inputSize: The number of inputs the layer will receive.
+//   - numNeurons: The number of neurons in the layer.
+//   - activation: The activation function to be used in the layer.
+//
+// Returns:
+//   - A pointer to an initialized Layer instance.
+//
+// Note:
+//   - Weights and biases are initialized with random values for start.
+//   - This function provides a general-purpose way to create layers of different types.
+
 func NewLayer(inputSize, numNeurons int, activation ActivationFunction) *Layer {
 	// Initialize layer with weights and biases
 	weights := make([][]float64, numNeurons)
@@ -40,6 +67,18 @@ func NewLayer(inputSize, numNeurons int, activation ActivationFunction) *Layer {
 		Activation: activation,
 	}
 }
+
+// Forward performs the forward pass for the layer.
+// It computes the output of the layer given an input.
+//
+// Arguments:
+//   - input: A slice of float64 representing the input to the layer.
+//
+// Returns:
+//   - A slice of float64 representing the output from the layer after applying the weights, biases, and activation function.
+//
+// Note:
+//   - Calculates the linear combination of inputs and weights, adds the bias, and applies the activation function.
 
 func (l *Layer) Forward(input []float64) []float64 {
 	// Forward pass
--- layer/recurrent.go
+++ layer/recurrent.go
@@ -1,9 +1,38 @@
 package layer
 
+// Recurrent layers are designed to process sequences of data by maintaining a 'memory'
+// of previous inputs through internal states.
+//
+// Fields:
+//   - Layer: A pointer to the base Layer struct (includes fields like weights, biases, and activation function).
+//   - TimeSteps: The number of time steps or sequence length the layer will unroll for during processing.
+//
+// Usage:
+//   - Recurrent layers are good for processing time-series data, natural language processing, and other sequence-related tasks.
+//
+// Note:
+//   - Unlike dense layers, recurrent layers share weights across time steps.
+
 type RecurrentLayer struct {
 	*Layer
 	TimeSteps int
 }
+
+// Rec initializes a new instance of a recurrent layer with specified parameters.
+//
+// Arguments:
+//   - inputSize: Number of input units to the layer (depends on the size of the previous layer or the feature space).
+//   - numNeurons: Number of neurons (or units) in the recurrent layer.
+//   - timeSteps: The number of time steps the layer should process.
+//   - activation: Activation function to be used in the layer.
+//
+// Returns:
+//   - A pointer to an initialized RecurrentLayer instance.
+//
+// Note:
+//   - The timeSteps parameter determines how many previous steps in the sequence the layer retains information from.
+//   - The activation function is applied to the output at each time step.
+//   - Sets up the initial structure of the recurrent layer but does not initialize weights and biases.
 
 func Rec(inputSize, numNeurons, timeSteps int, activation ActivationFunction) *RecurrentLayer {
 	layer := NewLayer(inputSize, numNeurons, activation)
