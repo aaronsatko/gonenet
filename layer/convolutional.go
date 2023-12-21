5e30d0211b5a402ce0c4dbb8a1ecc98b74853546
aaronsatko <agsatko@gmail.com>
2023-12-20 23:39:28 -0500

    documentation

--- layer/convolutional.go
+++ layer/convolutional.go
@@ -1,10 +1,42 @@
 package layer
 
+// ConvLayer represents a convolutional layer in a neural network.
+// Convolutional layers are primarily used in processing data with a grid-like topology (e.g., images).
+//
+// Fields:
+//   - Layer: A pointer to the base Layer struct (includes fields like weights, biases, and activation function).
+//   - KernelSize: Size of the convolutional kernel (filter).
+//   - Stride: Step size with which the kernel moves across the input data.
+//
+// Usage:
+//   - Convolutional layers are essential in Convolutional Neural Networks (CNNs) for tasks like image classification, object detection, etc.
+//   - They are effective in extracting features from local input patches and maintaining spatial relationships between pixels.
+//
+// Note:
+//   - The convolution operation involves sliding the kernel over the input and computing dot products.
+//   - KernelSize and Stride determine how the kernel moves across the input and how the output dimensions are calculated.
+
 type ConvLayer struct {
 	*Layer
 	KernelSize int
 	Stride     int
 }
+
+// Conv creates a new instance of a convolutional layer with specified parameters.
+//
+// Arguments:
+//   - inputSize: Number of input units to the layer (e.g., number of pixels in an image).
+//   - numNeurons: Number of neurons (or filters) in the convolutional layer.
+//   - kernelSize: Size of the convolutional kernel (filter).
+//   - stride: Step size for moving the kernel over the input data.
+//   - activation: Activation function to be used in the layer.
+//
+// Returns:
+//   - A pointer to an initialized ConvLayer instance.
+//
+// Note:
+//   - The activation function is applied to the output of the convolution operation.
+//   - This function sets up the initial structure of the convolutional layer but does not initialize weights and biases.
 
 func Conv(inputSize, numNeurons, kernelSize, stride int, activation ActivationFunction) *ConvLayer {
 	layer := NewLayer(inputSize, numNeurons, activation)
--- layer/dense.go
+++ layer/dense.go
@@ -1,8 +1,37 @@
 package layer
 
+// DenseLayer represents a fully connected (dense) layer in a neural network.
+// In a dense layer, each neuron receives input from all neurons in the previous layer.
+//
+// Fields:
+//   - Layer: A pointer to the base Layer struct (includes fields like weights, biases, and activation function).
+//
+// Usage:
+//   - Dense layers are versatile and can be used in almost any neural network architecture,
+//     including deep learning for tasks like classification and regression.
+//
+// Note:
+//   - The dense layer is a crucial building block in neural networks and is often used in both the hidden layers
+//     and the output layer of a network.
+
 type DenseLayer struct {
 	*Layer
 }
+
+// Dense creates a new instance of a dense (fully connected) layer with the specified parameters.
+// It initializes a layer with a given number of neurons and an activation function.
+//
+// Arguments:
+//   - inputSize: Number of input units to the layer (depends on the size of the previous layer or the feature space).
+//   - numNeurons: Number of neurons in the dense layer.
+//   - activation: Activation function to be used in the layer.
+//
+// Returns:
+//   - A pointer to an initialized DenseLayer instance.
+//
+// Note:
+//   - The activation function is applied to the linear combination of weights and inputs of the layer.
+//   - This function sets up the initial structure of the dense layer but does not initialize weights and biases.
 
 func Dense(inputSize, numNeurons int, activation ActivationFunction) *DenseLayer {
 	layer := NewLayer(inputSize, numNeurons, activation)
